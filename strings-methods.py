43abfd1398e4a90d009f1f99169b87cc65d32d42
ibrhmies <02220201044@ogr.inonu.edu.tr>
2022-10-30 15:13:08 +0300

    Python-String-Methods
    Python'da strings metotları.

---
+++ strings-methods.py
@@ -0,0 +1,33 @@
+msg = 'Benim adım İbrahim SEVEN. Tanıştığımıza memnun oldum.'
+
+#sonuc = msg.upper() upper komutu değişkendeki bütün harfleri büyük harf yapar.
+
+#sonuc = msg.lower() lower komutu değişkendeki bütün harfleri küçük harf yapar.
+
+#sonuc = msg.title() title komutu değişkendeki her kelimenin baş harfini büyük harf yapar.
+
+#sonuc = msg.capitalize() capitalize komutu değişkendeki cümlenin sadece ilk kelimesini büyük harfle başlatır.
+
+#sonuc = msg.islower() is başına geldiği komutu soruya çevirir islower değişkendeki bütün harfler küçük mü sorusuna karşılık gelir cevap true veya false olarak karşımıza çıkar.
+
+#sonuc = msg.split() split komutu değişkendeki her kelimeyi ayrı ayrı gösterir. (['Benim', 'adım', 'İbrahim', 'SEVEN.', 'Tanıştığımıza', 'memnun', 'oldum.']) şeklinde.
+
+#sonuc = '    abc    '.strip() strip komutu verilen str karakterinin önünde ve arkasında olan bütün boşlukları kaldırır.
+
+#sonuc = msg.split()
+#sonuc = '-'.join(sonuc) join komutu splitle ayrılan kelimelerin arasına simgeyi,harfi,işareti vb. katar.
+
+#index = msg.index('memnun') index komutuyla değişkendeki herhangi bir kelimenin kaçıncı index ten itibaren başladığını gösterir bize.
+#print(index)
+
+#sonuc = msg.startswith('B') startswith komutu değişkenin hangi harfle başladığının sorulduğu true veya false diye cevaplayan komuttur.
+#print(sonuc)
+
+#sonuc = msg.endswith('.') endswith komutu değişkenin hangi harfle,simgeyle vb. bittiğinin sorulduğu true veya false diye cevaplayan komuttur.
+#print(sonuc)
+
+#sonuc = msg.replace('adım','ismim') replace komutu değişkendeki bir kelimeyi,harfi,simgeyi vb. başka bir kelime,simge,harfle değiştiren komuttur.
+#ilk yazılanı ikinciyle değiştirir.
+#print(sonuc)
+
+
